b86b20344a075ae149a0721ad7b7280508853d99
Yupin-Dasri <74158072+Yupin-Dasri@users.noreply.github.com>
2020-11-12 11:22:03 -0500

    java is cool!

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -2,5 +2,7 @@ public class Cocomelon {
     public static void main(String[] args) {
         //It's raining outside
         //It's so cold outside
+        //Java is cool!
+        //but also really complicated
     }
 }
